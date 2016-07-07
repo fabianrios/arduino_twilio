fb4defc64fbf8c2ea8722a91af0c9b51068bf0f5
fabianrios <faben02@gmail.com>
2016-07-07 13:21:22 +0200

    changes to twilio basic call

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+var serialport = require('serialport');// include the serial port library
+   SerialPort = serialport.SerialPort; // make a local instance of it
+   portName = process.argv[2];
 var path = require('path');
 var express = require('express');
 var morgan = require('morgan');
@@ -8,8 +11,54 @@ var config = require("../config");
 // Create a Twilio REST API client for authenticated requests to Twilio
 var client = twilio(config.accountSid, config.authToken);
 
+
+var myPort = new SerialPort(portName, {
+   baudRate: 9600,
+   // look for return and newline at the end of each data packet:
+   parser: serialport.parsers.readline("\n")
+ });
+ 
+
+
 // Configure application routes
 module.exports = function(app) {
+    // serial stuff
+    myPort.on('open', showPortOpen);
+    myPort.on('data', sendSerialData);
+    myPort.on('close', showPortClose);
+    myPort.on('error', showError);
+
+    function showPortOpen() {
+       console.log('port open. Data rate: ' + myPort.options.baudRate);
+    }
+
+    function sendSerialData(data) {
+       console.log("data:", data);
+       if (data == 430){
+         var url = 'https://boiling-gorge-96626.herokuapp.com/outbound';
+         client.makeCall({
+             to: '+4915170367787',
+             from: config.twilioNumber,
+             url: url
+         }, function(err, message) {
+             console.log(err);
+             if (err) {
+                 console.log(err)
+             } else {
+                 console.log('Thank you! We will be calling you shortly.');
+             }
+         });
+       }
+    }
+
+    function showPortClose() {
+       console.log('port closed.');
+    }
+
+    function showError(error) {
+       console.log('Serial port error: ' + error);
+    }
+    
     // Set Jade as the default template engine
     app.set('view engine', 'jade');
 
@@ -35,7 +84,8 @@ module.exports = function(app) {
         // This should be the publicly accessible URL for your application
         // Here, we just use the host for the application making the request,
         // but you can hard code it or use something different if need be
-        var url = 'http://' + request.headers.host + '/outbound';
+        console.log("request.headers.host", request.headers.host);
+        var url = 'https://boiling-gorge-96626.herokuapp.com/outbound';
         
         // Place an outbound call to the user, using the TwiML instructions
         // from the /outbound route
